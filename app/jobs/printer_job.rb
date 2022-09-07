671bb6815ce339302e599adb5bdf396d6eaa038e
Ameer Hamza <ameer.hamza@devbox.co>
2022-09-07 19:14:27 +0500

    Select printer

--- app/jobs/printer_job.rb
+++ app/jobs/printer_job.rb
@@ -12,25 +12,25 @@ class PrinterJob < ApplicationJob
     response = HTTParty.get(url)
     return 'Files not found' unless response.success? && response['files'].present?
 
-    save_response(response['files'])
+    save_response(response['files'], response['printer_name'])
   end
 
-  def save_response(files)
+  def save_response(files, printer_name)
     printer_label_ids = []
     files.each do |file|
       path = Rails.root.join('public', file.second.to_s)
       File.open(path.to_s, 'wb') do |f|
         f.write(Base64.decode64(file.first))
       end
-      print_file(path)
+      print_file(path, printer_name)
       printer_label_ids << file.last
     end
     UpdatePrinterLabelJob.perform_now(printer_label_ids: printer_label_ids)
   end
 
-  def print_file(path)
-    printers = CupsPrinter.get_all_printer_names
-    printer = CupsPrinter.new(printers.last)
+  def print_file(path, printer_name)
+    # printers = CupsPrinter.get_all_printer_names
+    printer = CupsPrinter.new(printer_name)
     printer.print_file(path.to_s, { 'PageSize' => 'A6' })
   end
 end
---
+++ app/jobs/set_printer_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# post available printers by api
+class UpdatePrinterLabelJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    printers = CupsPrinter.get_all_printer_names
+
+    data = { 'printers[]': printers }
+    url = "https://gettabox.channeldispatch.co.uk//api/v1/available_printers?#{URI.encode_www_form(data)}"
+
+    HTTParty.get(url)
+    # self.class.set(wait: 300.seconds).perform_later(printer_label_ids: id) unless response.success?
+  end
+end
